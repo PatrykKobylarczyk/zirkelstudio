24ad2224040044d1346dc13a001c4311c2432ca8
Patryk Kobylarczyk <patryk.kobylarczyk@gmail.com>
2023-04-11 22:05:50 +0200

    works with ios rwd

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="w-full h-screen grid place-items-center relative">
+      <main className="w-full md:h-screen grid place-items-center relative">
         {loading && <Loader />}
         <Carousel />
       </main>
