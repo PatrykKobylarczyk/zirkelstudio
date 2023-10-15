481982b290b1a125ac7e39ff18ae18252fb81348
Patryk Kobylarczyk <patryk.kobylarczyk@gmail.com>
2023-10-15 18:26:23 +0200

    add new photos

--- data/ImageData.tsx
+++ data/ImageData.tsx
@@ -1,21 +1,27 @@
 import photo00 from "../assets/00.jpg";
 import photo01 from "../assets/01.jpg";
+import photo001 from "../assets/001.jpg";
 import photo02 from "../assets/02.jpg";
 import photo03 from "../assets/03.jpg";
 import photo04 from "../assets/04.jpg";
+import photo004 from "../assets/004.jpg";
 import photo05 from "../assets/05.jpg";
 import photo06 from "../assets/06.jpg";
+import photo061 from "../assets/061.jpg";
 import photo07 from "../assets/07.jpg";
 import photo08 from "../assets/08.jpg";
 import photo09 from "../assets/09.jpg";
 import photo10 from "../assets/10.jpg";
+import photo101 from "../assets/101.jpg";
 import photo11 from "../assets/11.jpg";
 import photo12 from "../assets/12.jpg";
+import photo121 from "../assets/121.jpg";
 import photo13 from "../assets/13.jpg";
 import photo14 from "../assets/14.jpg";
 import photo15 from "../assets/15.jpg";
 import photo16 from "../assets/16.jpg";
 import photo17 from "../assets/17.jpg";
+import photo171 from "../assets/171.jpg";
 import photo18 from "../assets/18.jpg";
 import photo20 from "../assets/20.jpg";
 import photo21 from "../assets/21.jpg";
@@ -29,11 +35,13 @@ import photo28 from "../assets/28.jpg";
 import photo29 from "../assets/29.jpg";
 import photo30 from "../assets/30.jpg";
 import photo31 from "../assets/31.jpg";
+import photo311 from "../assets/311.jpg";
 import photo32 from "../assets/32.jpg";
 import photo33 from "../assets/33.jpg";
 import photo34 from "../assets/34.jpg";
 import photo35 from "../assets/35.jpg";
 import photo37 from "../assets/37.jpg";
+import photo371 from "../assets/371.jpg";
 
 export const images = [
   {
@@ -43,6 +51,10 @@ export const images = [
   {
     id: 1,
     path: photo01,
+  },
+  {
+    id: 101,
+    path: photo001,
   },
   {
     id: 2,
@@ -57,12 +69,20 @@ export const images = [
     path: photo04,
   },
   {
+    id: 404,
+    path: photo004,
+  },
+  {
     id: 5,
     path: photo05,
   },
   {
     id: 6,
     path: photo06,
+  },
+  {
+    id: 601,
+    path: photo061,
   },
   {
     id: 7,
@@ -81,12 +101,20 @@ export const images = [
     path: photo10,
   },
   {
+    id: 101,
+    path: photo101,
+  },
+  {
     id: 11,
     path: photo11,
   },
   {
     id: 12,
     path: photo12,
+  },
+  {
+    id: 121,
+    path: photo121,
   },
   {
     id: 13,
@@ -107,6 +135,10 @@ export const images = [
   {
     id: 17,
     path: photo17,
+  },
+  {
+    id: 171,
+    path: photo171,
   },
   {
     id: 18,
@@ -161,6 +193,10 @@ export const images = [
     path: photo31,
   },
   {
+    id: 311,
+    path: photo311,
+  },
+  {
     id: 32,
     path: photo32,
   },
@@ -179,6 +215,10 @@ export const images = [
   {
     id: 37,
     path: photo37,
+  },
+  {
+    id: 371,
+    path: photo371,
   },
 ];
 
